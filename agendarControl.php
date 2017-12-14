fbdf9f3725533945c02444a439fbf3e4025a4d94
Javiera <Javiera@LAPTOP-868K991T.home>
2017-12-14 18:01:29 -0300

    Actualizacion

--- agendarControl.php
+++ agendarControl.php
@@ -26,9 +26,9 @@
         <ul class="nav navbar-nav navbar-right">
           <li><a href="pacientes.php">Pacientes</a></li>
           <li><a href="estadisticas.php">Estadisticas</a></li>
-          <li><a href="mapa.php">Geolocalización</a></li>
+          <li><a href="mapa.php">Geolocalización</a></li> 
           <li><a href="formPac.php">Ingresar Paciente</a></li> 
-          <li><form action="formControl.php" method="post">
+          <li><form action="agendarControl.php" method="post">
                   <button type="submit" class="btn btn-green btn-block btn-flat" name="salir" style="margin-top:10%">Salir</button>
           </form>
           </li> 
@@ -43,119 +43,125 @@
           <?php
                 session_start();
                 
-                $rut=$_GET['agendar'];
-                require 'conexion.php';
-                $s="SELECT CodRFID FROM paciente WHERE Rut=$rut";
-                $r=$conn->query($s);
-
-                $row=$r->fetch_assoc();
-                
-                echo '<br><div class="row">
-                    <div class="col-md-1">
-                        <a href="ficha.php?tag='.$row['CodRFID'].'"><button type="submit" class="btn btn-primary btn-xs"> < </button></a>
-                        
-                    </div>
-                    <div class="col-md-11">
-                        <p style="text-align:right"><img src="img/blue.png" width="20" />'.$_SESSION['nombre'].'</p>
-                    </div>
-                </div>';
-                
                 if(isset($_POST['salir'])){
                     session_destroy();
                     header('Location: index.php');
                 }
-              ?>
-        <div class="row">
-          <div class="header-section text-center">
-            <h2>Registrar Hora Control</h2>           
-            
-                <?php 
-                    date_default_timezone_set('America/Santiago');
-                    $fecha= date('d-m-Y');
-                    $hora= time('H:i:s');
-                    require 'conexion.php';
-                    $sql="SELECT *FROM paciente WHERE Rut=$rut";
-                    $result=$conn->query($sql);
-                    if($result->num_rows>0){
-                        while($row=$result->fetch_assoc()){
-                            echo'
-            <hr class="bottom-line">
-          </div>
-          <div class="container">
-            <div class="row">
-                <div class="col-lg-10">
-                    
-                    <form id="form" method="post" class="form-horizontal mitad" action="agendarControl.php">
+                
+                $rut=$_GET['agendar'];
+                
+                date_default_timezone_set('America/Santiago');
+                $fecha= date('d-m-Y');
+                $hora= time('H:i:s');
+                
+                require 'conexion.php';
+                $s="SELECT * FROM paciente WHERE Rut=$rut";
+                $r=$conn->query($s);
+                if($r->num_rows>0){
+                    while($row=$r->fetch_assoc()){
+                        echo '<br>
+                                <div class="row">
+                                    <div class="col-md-1">
+                                        <a href="ficha.php?tag='.$row['CodRFID'].'"><button type="submit" class="btn btn-primary btn-xs"> < </button></a>
+                                    </div>
+                                    <div class="col-md-11">
+                                        <p style="text-align:right"><img src="img/blue.png" width="20" />'.$_SESSION['nombre'].'</p>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="header-section text-center">
+                                        <h2>Registrar Hora Control</h2>
+                                        <hr class="bottom-line">
+                                    </div>
+                                  
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-lg-10">
+
+                                                <form id="form" method="post" class="form-horizontal mitad" action="agendarControl.php">
+
+                                                    <div class="form-group row">
+                                                        <label class="col-xs-3 col-form-label">Nombre Paciente: </label>
+                                                        <div class="col-xs-9">'
+                                                            .$row['Nombre'].'
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group row">
+                                                        <label class="col-xs-3 col-form-label">Rut Paciente: </label>
+                                                        <div class="col-xs-9">'
+                                                            .$row['Rut'].'
+                                                        </div>
+                                                    </div>                      
+
+                                                    <div class="form-group row">
+                                                        <label class="col-xs-5 col-form-label">Se agendará un control Médico con el profesional: </label>
+                                                        <div class="col-xs-4">'
+                                                            .$_SESSION['nombre'].'
+                                                        </div>
+                                                    </div> 
+                                                    
+                                                    <input type="hidden" name="nombre" value="'.$row['Nombre'].'"></input>
+                                                    <input type="hidden" name="rut" value="'.$row['Rut'].'"></input>
+
+                                                    <div class="form-group row">
+                                                        <label class="col-xs-2 col-form-label">Fecha Control: </label>
+                                                        <div class="col-xs-10">
+                                                            <input class="form-control" type="date"  name="fecha" placeholder="Motivo por el cual el paciente Consulta" />
+                                                        </div>
+                                                    </div>  
+
+                                                    <div class="form-group row">
+                                                        <label class="col-xs-2 col-form-label">Hora Control: </label>
+                                                        <div class="col-xs-10">
+                                                            <input class="form-control" type="time" name="hora" placeholder="Motivo por el cual el paciente Consulta" />
+                                                        </div>
+                                                    </div>  
+
+                                                    <div class="col-xs-12">
+                                                        <button type="submit" id="submit" name="submit" class="form contact-form-button light-form-button oswald light">Registrar Control</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>';
+                            }
+
+                        }
+
+                        $conn->close();
                         
-                        <div class="form-group row">
-                            <label class="col-xs-3 col-form-label">Nombre Paciente: </label>
-                            <div class="col-xs-9">'
-                                .$row['Nombre'].'
-                            </div>
-                        </div>
+                        if(isset($_POST['submit'])){
+                            $name = $_POST['nombre'];
+                            $rutPac = $_POST['rut'];
+                            $nombreMed = $_SESSION['nombre'];  
+                            $fechaControl = date_create($_POST['fecha']);
+                            $fechaControl = date_format($fechaControl, 'd-m-Y');
+                            $horacontrol = $_POST['hora'];
+                            require "conexion.php";            
+                            $sql= "insert into agendarcontrol values ('$name',$rutPac, '$nombreMed', '$fechaControl', '$horacontrol')";
+                            $result = $conn->query($sql);
+                            if($conn->affected_rows==0){                
+                                echo "<script>alert('No se pudo ingresar el control')</script>";
+                            }else{
+                                echo "<script>alert('Control Ingresado')</script>";
+                            }
                             
-                        <div class="form-group row">
-                            <label class="col-xs-3 col-form-label">Rut Paciente: </label>
-                            <div class="col-xs-9">'
-                                .$row['Rut'].'
-                            </div>
-                        </div>                      
+                            echo "<script language='javascript'>
+                                    window.location='pacientes.php';
+                                    window.opener.location.reload();
+                                    window.close();
+                                  </script>";
+                            $conn->close();
 
-                        <div class="form-group row">
-                            <label class="col-xs-5 col-form-label">Se agendará un control Médico con el profesional: </label>
-                            <div class="col-xs-4">'
-                                .$_SESSION['nombre'].'
-                            </div>
-                        </div> 
+                        }   
 
-                        <div class="form-group row">
-                            <label class="col-xs-2 col-form-label">Fecha Control: </label>
-                            <div class="col-xs-10">
-                                <input class="form-control" type="date"  name="fecha" placeholder="Motivo por el cual el paciente Consulta" />
-                            </div>
-                        </div>  
-                     
-                        <div class="form-group row">
-                            <label class="col-xs-2 col-form-label">Hora Control: </label>
-                            <div class="col-xs-10">
-                                <input class="form-control" type="time"  name="hora" placeholder="Motivo por el cual el paciente Consulta" />
-                            </div>
-                        </div>  
+                    ?>  
 
-                        <div class="col-xs-12">
-                            <button type="submit" id="submit" name="submit" class="form contact-form-button light-form-button oswald light">Registrar Control</button>
-                        </div>
-                    </form>';
-    }
-    
-    }else{
-        echo '<script>alert("No se pudo ingresar el control");</script>';
-    }
-    
-     $conn->close();
-     
-     $rut=$_GET['agendar'];
-        
-        if(isset($_POST['submit'])){
-            $name = $row['Nombre'];
-            $rutPac = $row['Rut'];
-            $nombreMed = $_SESSION['nombre'];  
-            $fechaControl = $_POST['fecha'];
-            $horacontrol = $_POST['hora'];
-            require "conexion.php";            
-            $sql= "Insert into agendarcontrol (nombrePac, rutPac, nombreMed, fecha, hora) values ('$name','$rutPac', '$nombreMed', '$fechaControl', '$horacontrol')";
-            $result = $conn->query($sql);
-            $conn->close();
-            header('Location: /MediSud/pacientes.php');
-        }   
-     
-     ?>  
-
-                </div>
+                
             </div>
-        </div>
-        </div>
-      </div>
     </section>
     <!--/ Contact-->
     <!--Footer-->
